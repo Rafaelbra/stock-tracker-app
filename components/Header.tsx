2fdd509b66fde3038fcc765620189bbad8cb6f93
Rafael Braga <rafaelbra149@gmail.com>
2025-09-29 15:04:17 -0400

    Introducing NavItems to Navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import NavItems from "@/components/NavItems";
 
 const Header = () => {
     return (
@@ -9,8 +10,9 @@ const Header = () => {
                     <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
                 </Link>
                 <nav className="hidden sm:block">
-
+                    <NavItems />
                 </nav>
+                {/*<UserDropdown />*/}
             </div>
         </header>
     )
